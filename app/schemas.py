6929263fab6ce354b1535c425ab541ef1869c708
Kauan <kauanpsilva66@gmail.com>
2025-07-03 20:45:16 -0300

    Creation of Login and Token schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel, EmailStr
 
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
