fa2e70b4a15344c1fc4ffc851898e17a77e6ebf5
Kauan <kauanpsilva66@gmail.com>
2025-07-03 20:44:47 -0300

    Creation of route /login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app import models, schemas
 from app.database import get_db
-from app.auth import utils
+from app.auth import utils, jwt
+
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -21,3 +22,21 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     
     return new_user
+
+
+@router.post("/login", response_model=schemas.TokenResponse)
+def login(login_data: schemas.LoginRequest, db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(models.User.email == login_data.email).first()
+
+    if not user or not utils.verify_password(login_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid Credencials")
+    
+    access_token = jwt.create_access_token(data={"sub": str(user.id)})
+    refresh_token = jwt.create_refresh_token(data={"sub": str(user.id)})
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer"
+    }
